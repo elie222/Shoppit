fb9cb2dd5bb45d8a40a01b4d41ddd6aae6561716
Eliezer <Elie2@elies-macbook-2.local>
2013-08-25 21:09:00 +0100

    wrote the log in code. haven't tested it. probably works

--- shoppit/gen/il/ac/huji/shoppit/R.java
+++ shoppit/gen/il/ac/huji/shoppit/R.java
@@ -997,15 +997,15 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f05002f;
         public static final int action_mode_bar_stub=0x7f05002e;
         public static final int action_mode_close_button=0x7f050022;
-        public static final int action_search=0x7f050056;
-        public static final int action_settings=0x7f050055;
+        public static final int action_search=0x7f05005b;
+        public static final int action_settings=0x7f05005a;
         public static final int activity_chooser_view_content=0x7f050023;
         public static final int add=0x7f050048;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f050053;
-        public static final int button2=0x7f050054;
-        public static final int button3=0x7f050052;
+        public static final int button1=0x7f050058;
+        public static final int button2=0x7f050059;
+        public static final int button3=0x7f050057;
         public static final int checkbox=0x7f05002b;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050026;
@@ -1024,17 +1024,19 @@ containing a value of this type.
         public static final int image=0x7f050025;
         public static final int itemList=0x7f050046;
         public static final int left_icon=0x7f050031;
-        public static final int list1=0x7f05004f;
-        public static final int list2=0x7f050050;
+        public static final int list1=0x7f050054;
+        public static final int list2=0x7f050055;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050027;
+        public static final int logInBtn=0x7f05004f;
+        public static final int logInPasswordEdt=0x7f05004e;
+        public static final int logInUsernameEdt=0x7f05004c;
         public static final int map=0x7f050049;
         public static final int middle=0x7f050012;
-        public static final int name=0x7f05004e;
+        public static final int name=0x7f050053;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int passwordEdt=0x7f05004c;
         public static final int progress_circular=0x7f050034;
         public static final int progress_horizontal=0x7f050035;
         public static final int radio=0x7f05002d;
@@ -1055,20 +1057,23 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
-        public static final int signUpBtn=0x7f05004d;
+        public static final int signUpBtn=0x7f050052;
+        public static final int signUpPasswordEdt=0x7f050051;
+        public static final int signUpUsernameEdt=0x7f050050;
         public static final int sorter=0x7f050045;
         public static final int split_action_bar=0x7f05001c;
         public static final int submit_area=0x7f05003f;
-        public static final int surfaceView1=0x7f050051;
+        public static final int surfaceView1=0x7f050056;
         public static final int tabMode=0x7f050002;
         public static final int textView1=0x7f050043;
         public static final int textView2=0x7f050044;
+        public static final int textView3=0x7f05004b;
+        public static final int textView4=0x7f05004d;
         public static final int title=0x7f050029;
         public static final int title_container=0x7f050030;
         public static final int top_action_bar=0x7f05001e;
         public static final int up=0x7f05001f;
         public static final int useLogo=0x7f050003;
-        public static final int usernameEdt=0x7f05004b;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1180,6 +1185,7 @@ containing a value of this type.
         public static final int barcode=0x7f0a0015;
         public static final int categ=0x7f0a0012;
         public static final int hello_world=0x7f0a000f;
+        public static final int logIn=0x7f0a001b;
         public static final int map=0x7f0a0014;
         public static final int nearby_items=0x7f0a0010;
         public static final int password=0x7f0a0019;
@@ -1187,7 +1193,7 @@ containing a value of this type.
         public static final int signUp=0x7f0a001a;
         public static final int skip=0x7f0a0016;
         public static final int soty_by=0x7f0a0011;
-        public static final int title_activity_main_activity_new=0x7f0a001b;
+        public static final int title_activity_main_activity_new=0x7f0a001c;
         public static final int username=0x7f0a0018;
     }
     public static final class style {
--- shoppit/src/il/ac/huji/shoppit/MainActivityNew.java
+++ shoppit/src/il/ac/huji/shoppit/MainActivityNew.java
@@ -7,15 +7,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
-import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
 public class MainActivityNew extends ActionBarActivity {
 	
+	private Button logInButton;
 	private Button signUpButton;
 
 	@Override
@@ -31,6 +32,14 @@ public class MainActivityNew extends ActionBarActivity {
 //		ParseObject testObject = new ParseObject("TestObject");
 //		testObject.put("foo", "bar");
 //		testObject.saveInBackground();
+		
+		logInButton = (Button) findViewById(R.id.logInBtn);
+		logInButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onLogInButtonClicked();
+			}
+		});
 		
 		signUpButton = (Button) findViewById(R.id.signUpBtn);
 		signUpButton.setOnClickListener(new View.OnClickListener() {
@@ -48,10 +57,29 @@ public class MainActivityNew extends ActionBarActivity {
 		return true;
 	}
 	
+	private void onLogInButtonClicked() {
+		
+		EditText usernameEdt = (EditText) findViewById(R.id.logInUsernameEdt);
+		EditText passwordEdt = (EditText) findViewById(R.id.logInPasswordEdt);
+
+		String username = usernameEdt.getText().toString();
+		String password = passwordEdt.getText().toString();
+		
+		ParseUser.logInInBackground(username, password, new LogInCallback() {
+			  public void done(ParseUser user, ParseException e) {
+			    if (user != null) {
+			      // Hooray! The user is logged in.
+			    } else {
+			      // Signup failed. Look at the ParseException to see what happened.
+			    }
+			  }
+			});
+	}
+	
 	private void onSignUpButtonClicked() {
 		
-		EditText usernameEdt = (EditText) findViewById(R.id.usernameEdt);
-		EditText passwordEdt = (EditText) findViewById(R.id.passwordEdt);
+		EditText usernameEdt = (EditText) findViewById(R.id.signUpUsernameEdt);
+		EditText passwordEdt = (EditText) findViewById(R.id.signUpPasswordEdt);
 
 		String username = usernameEdt.getText().toString();
 		String password = passwordEdt.getText().toString();
