459926379af7cc2e6d0cb24721c8ef70995f1d8b
Eliezer <Elie2@elies-macbook-2.local>
2013-08-25 20:24:36 +0100

    set up parse. created a test object and it works

--- shoppit/src/il/ac/huji/shoppit/MainActivityNew.java
+++ shoppit/src/il/ac/huji/shoppit/MainActivityNew.java
@@ -3,6 +3,9 @@ package il.ac.huji.shoppit;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
 
 public class MainActivityNew extends ActionBarActivity {
 
@@ -10,6 +13,14 @@ public class MainActivityNew extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_activity_new);
+		
+		Parse.initialize(this, "jAcoqyTFZ83HhbvfAaGQUe9hcu8lf0IOhyyYVKj5", "6gYN5nmVPMPpwyL0qNLOJbqShosYV0JR7Owp2Oli"); 
+		
+		ParseAnalytics.trackAppOpened(getIntent());
+		
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
 	}
 
 	@Override
