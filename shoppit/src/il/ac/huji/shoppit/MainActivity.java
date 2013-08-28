13a56967f44e85e1e39f533709b41ddcd213319c
Eliezer <Elie2@elies-macbook-2.local>
2013-08-28 12:04:01 +0100

    commented out some ugliness in MainActivity. Can replace it with tabs or something discrete that lets the user choose between sorting options.

--- shoppit/src/il/ac/huji/shoppit/MainActivity.java
+++ shoppit/src/il/ac/huji/shoppit/MainActivity.java
@@ -13,17 +13,17 @@ import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
 import android.os.Bundle;
-import android.app.Activity;
+//import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.AdapterView.OnItemSelectedListener;
+//import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Spinner;
+//import android.widget.Spinner;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -31,8 +31,8 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-//		ParseObject.registerSubclass(Item.class);
+
+		//		ParseObject.registerSubclass(Item.class);
 		Parse.initialize(this, "jAcoqyTFZ83HhbvfAaGQUe9hcu8lf0IOhyyYVKj5", "6gYN5nmVPMPpwyL0qNLOJbqShosYV0JR7Owp2Oli");
 
 		//Check if the user is logged in, connect to parse if so.
@@ -49,31 +49,31 @@ public class MainActivity extends ActionBarActivity {
 		//TODO - not working yet
 		//HomeListAdapter adapter = new HomeListAdapter(this, new ParseQueryAdapter.QueryFactory<ParseObject>() {
 		ParseQueryAdapter<ParseObject> adapter = new ParseQueryAdapter<ParseObject>(this, new ParseQueryAdapter.QueryFactory<ParseObject>() {
-				    public ParseQuery<ParseObject> create() {
-				      // Here we can configure a ParseQuery to our heart's desire.
-				      ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("TestObject");
-				      // you can do a more advanced query as follows:
-				      // can also set an order for the items.
-//				      query.whereContainedIn("genre", Arrays.asList({ "Punk", "Metal" }));
-//				      query.whereGreaterThanOrEqualTo("memberCount", 4);
-//				      query.orderByDescending("albumsSoldCount");
-				      return query;
-				    }
-				  });
-		
-//		ParseQueryAdapter<ParseObject> adapter = new ParseQueryAdapter<ParseObject>(this, "TestObject");
+			public ParseQuery<ParseObject> create() {
+				// Here we can configure a ParseQuery to our heart's desire.
+				ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("TestObject");
+				// you can do a more advanced query as follows:
+				// can also set an order for the items.
+				//				      query.whereContainedIn("genre", Arrays.asList({ "Punk", "Metal" }));
+				//				      query.whereGreaterThanOrEqualTo("memberCount", 4);
+				//				      query.orderByDescending("albumsSoldCount");
+				return query;
+			}
+		});
+
+		//		ParseQueryAdapter<ParseObject> adapter = new ParseQueryAdapter<ParseObject>(this, "TestObject");
 		adapter.setTextKey("name");
 		adapter.setImageKey("photo");
 		ListView listView = (ListView) findViewById(R.id.homeListView);
 		listView.setAdapter(adapter);
 
 		//Create the sorter.
-		Spinner spinner = (Spinner) findViewById(R.id.sorter);
-		ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
-				this, R.array.sort_options, android.R.layout.simple_spinner_item);
-		spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner.setAdapter(spinnerAdapter);
-		spinner.setOnItemSelectedListener(new Sorter());
+//		Spinner spinner = (Spinner) findViewById(R.id.sorter);
+//		ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
+//				this, R.array.sort_options, android.R.layout.simple_spinner_item);
+//		spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//		spinner.setAdapter(spinnerAdapter);
+//		spinner.setOnItemSelectedListener(new Sorter());
 	}
 
 	@Override
@@ -98,8 +98,16 @@ public class MainActivity extends ActionBarActivity {
 
 				ParseUser.logInInBackground(username, password, new LogInCallback() {
 					public void done(ParseUser user, ParseException e) {
-						GeneralInfo.logged = (user != null);
-						GeneralInfo.username = username;
+						
+						if (e == null && user != null) {
+//							loginSuccessful();
+							GeneralInfo.logged = true;
+							GeneralInfo.username = username;
+						} else if (user == null) {
+//							usernameOrPasswordIsInvalid();
+						} else {
+//							somethingWentWrong();
+						}
 					}
 				});
 
@@ -107,7 +115,7 @@ public class MainActivity extends ActionBarActivity {
 			try {
 				reader.close();
 			} catch (Exception e) {}
-			
+
 		}
 	}
 
@@ -133,15 +141,15 @@ public class MainActivity extends ActionBarActivity {
 
 
 	//This class implements sorting nearby items.
-	private class Sorter extends Activity implements OnItemSelectedListener {
-
-		public void onItemSelected(AdapterView<?> parent, View view, 
-				int pos, long id) {
-			// pos: 0 = cheapest, 1 = closest, 2 = newest.
-		}
-
-		public void onNothingSelected(AdapterView<?> parent) {}
-	}
+//	private class Sorter extends Activity implements OnItemSelectedListener {
+//
+//		public void onItemSelected(AdapterView<?> parent, View view, 
+//				int pos, long id) {
+//			// pos: 0 = cheapest, 1 = closest, 2 = newest.
+//		}
+//
+//		public void onNothingSelected(AdapterView<?> parent) {}
+//	}
 
 
 	private void addItemIfLoggedIn() {
@@ -151,19 +159,19 @@ public class MainActivity extends ActionBarActivity {
 			startActivity(new Intent(getBaseContext(), TakePictureActivity.class));
 			return;
 		}
-		
+
 		//Else, ask the user to log in.
 		Intent intent = new Intent(getBaseContext(), LoginActivity.class);
 		startActivityForResult(intent, 5000);
 	}
-	
-	
+
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 		//If user logged in successfully, continue to taking the item picture.
-	    if (requestCode == 5000 && resultCode == RESULT_OK){
-	    	startActivity(new Intent(getBaseContext(), TakePictureActivity.class));
+		if (requestCode == 5000 && resultCode == RESULT_OK){
+			startActivity(new Intent(getBaseContext(), TakePictureActivity.class));
 		}
 	}
 
